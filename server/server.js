0bfdb89def1fa96660f2c6fdb96fb378c8dc209b
Christian <chrishamaoui@hotmail.com>
2021-07-07 00:45:41 -0400

    Create API key, add .env axios and create basic api requests

--- server/server.js
+++ server/server.js
@@ -20,5 +20,5 @@ app.get('/favourite-movies-list', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log('Server running on port: ' + port);
+  console.log(`Server running on port: ${port}`);
 });
